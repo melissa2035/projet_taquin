f669a2e291bde415df6d1d1256f8c126c36bea5b
melissa2035 <115987319+melissa2035@users.noreply.github.com>
2023-04-20 23:11:01 +0200

    Update taquin.py

--- taquin.py
+++ taquin.py
@@ -1,41 +1,72 @@
 #Le jeu consiste en un puzzle constituait de 15 carrés numerotés  de 1  à 15, dans lequel une première configuration aléatoie est donné.
-#On nous demande donc de creer une interface graphique pour y jouer; on essayera egalement d'integrer des elements permettant de sauvegarder ou  bien recharger une partie, mais aussi un element permettant de remelanger la configuaration de base en cas ou non solutio
+#On nous demande donc de creer une interface graphique pour y jouer; on essayera egalement d'integrer des elements permettant de sauvegarder ou  bien recharger une partie.
 
 import random
 import tkinter as tk
 
-#Initialisation du plateau de jeu
-tableau = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, None]]
+# Initialisation du plateau de jeu
+board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, None]]
 
-#fonction pour mélanger les tuiles
-def shuffle_tableau(tableau):
-    flatten_tableau = [tile for row in tableau for  tile in row]
-    random.shuffle(flatten_tableau)
-    shuffled_tableau = [[flatten_tableau.pop(0)for _ in row] for row in tableau]
-    return shuffled_tableau
+# Mélanger les tuiles
+def shuffle_board(board):
+    flatten_board = [tile for row in board for tile in row]
+    random.shuffle(flatten_board)
+    shuffled_board = [[flatten_board.pop(0) for _ in row] for row in board]
+    return shuffled_board
 
-#Le joueur a gagné
-def a_gagne(tableau):
-    return tableau == [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, None]]
+# Vérifier si le joueur a gagné
+def is_winner(board):
+    return board == [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, None]]
 
 # Obtenir la position de la tuile vide
-def case_vide(tableau):
+def get_blank_pos(board):
     for i in range(4):
         for j in range(4):
             if board[i][j] == None:
                 return (i, j)
 
 # Déplacer la tuile vers la position vide
-def  deplace_tuile(tableau, tuile_position):
-    vide_c = case_vide(tableau)
-    tableau[tuile_position[0]][tuile_position[1]], tableau[vide_c[0]][vide_c[1]] = tableau[vide_c[0]][vide_c[1]], tableau[tuile_position[0]][tuile_position[1]]
-    return tableau
+def move_tile(board, tile_pos):
+    blank_pos = get_blank_pos(board)
+    board[tile_pos[0]][tile_pos[1]], board[blank_pos[0]][blank_pos[1]] = board[blank_pos[0]][blank_pos[1]], board[tile_pos[0]][tile_pos[1]]
+    return board
 
 # Créer la fenêtre graphique
-fenetre = tk.Tk()
-fenetre.title("Solitaire du taquin")
+window = tk.Tk()
+window.title("Solitaire du taquin")
 
 # Initialiser le plateau de jeu
-tableau = shuffle_tableau(tableau)
+board = shuffle_board(board)
 
-fenetre.mainloop()
+# Créer la grille de boutons
+buttons = []
+for i in range(4):
+    row = []
+    for j in range(4):
+        button = tk.Button(window, text=str(board[i][j]), font=("Arial", 20), width=4, height=3)
+        button.grid(row=i, column=j)
+        row.append(button)
+    buttons.append(row)
+
+# Fonction pour mettre à jour le texte des boutons et l'état du plateau
+def update_board(board):
+    for i in range(4):
+        for j in range(4):
+            buttons[i][j].config(text=str(board[i][j]))
+
+# Fonction pour gérer les clics de boutons
+def on_click(i, j):
+    global board
+    tile_pos = (i, j)
+    board = move_tile(board, tile_pos)
+    update_board(board)
+    if is_winner(board):
+        tk.messagebox.showinfo("Félicitations", "Vous avez gagné le jeu !")
+
+# Associer les boutons à la fonction de clic
+for i in range(4):
+    for j in range(4):
+        buttons[i][j].config(command=lambda i=i, j=j: on_click(i, j))
+
+# Démarrer la boucle principale des événements
+window.mainloop()
